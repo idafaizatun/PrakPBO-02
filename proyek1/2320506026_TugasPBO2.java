204c87f1b583d61cfceb05cf5995f68fe49bb892
idafaizatun <ida.faizatun.zakiyah@students.untidar.ac.id>
2024-08-29 11:35:09 +0700

    Tugas PBO2

---
+++ proyek1/2320506026_TugasPBO2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ida_project1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Ida_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Ida Faizatun Zakiyah");
+        System.out.println("NPM : 2320506026");
+        System.out.println("Alamat : Banjarnegara");
+        System.out.println("No_Hp : 085291633079");
+    }
+}
